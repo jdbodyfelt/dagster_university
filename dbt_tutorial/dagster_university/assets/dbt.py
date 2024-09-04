67297767a43f862acd357be629734d440fff2e34
jdbodyfelt <jdbodyfelt@gmail.com>
2024-09-04 18:11:50 +1200

    Lesson 5 Done

--- dbt_tutorial/dagster_university/assets/dbt.py
+++ dbt_tutorial/dagster_university/assets/dbt.py
@@ -1,11 +1,42 @@
-from dagster import AssetExecutionContext
-from dagster_dbt import dbt_assets, DbtCliResource
+from dagster import AssetExecutionContext, AssetKey
+from dagster_dbt import dbt_assets, DbtCliResource, DagsterDbtTranslator
 
 from ..project import dbt_project
 
+
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    """
+    A customized dbt translator that overrides the get_asset_key method 
+    to return a custom asset key for dbt sources.
+    """
+    def get_asset_key(self, dbt_resource_props):
+        """
+        Get the asset key for a dbt resource.
+        
+        Args:
+            dbt_resource_props (dict): The properties of the dbt resource.
+        
+        Returns:
+            AssetKey: The asset key.
+        """
+        rtype = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+        if rtype == "source":
+            key_ = AssetKey(f"taxi_{name}")
+        else:
+            key_ = super().get_asset_key(dbt_resource_props)
+        return key_
+    
+    def get_group_name(self, dbt_resource_props):
+        return dbt_resource_props["fqn"][1]
+
+       
+
 @dbt_assets(
     manifest=dbt_project.manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator()
 )
 def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
+    """"""
     yield from dbt.cli(["build"], context=context).stream()
     
--- openQuestions.md
+++ openQuestions.md
@@ -1,2 +1,3 @@
 1. dbt and Dagster assume the DAG and dbt project share the same code location. What if the dbt build is in another repository? Do we need to pull in locally?
 2. Do the providers have any solutions to this? (e.g. dagster-dbt)
+3. How does the DAG graph in dbt docs get translated into Dagster graphs?
